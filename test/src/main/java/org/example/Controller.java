ed3476806568568612b909b9b470105c57be653e
pocwicz1 <dawid.halat24@gmail.com>
2026-03-12 21:31:13 +0100

    67

--- test/src/main/java/org/example/Controller.java
+++ test/src/main/java/org/example/Controller.java
@@ -3,6 +3,9 @@ package org.example;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.FileWriter;
+import java.io.IOException;
+
 import java.util.HashMap;
 import java.util.Map;
 
@@ -42,6 +45,21 @@ public class Controller {
         response.put("sum", String.valueOf(mathService.add(a, b)));
         response.put("multiplication", String.valueOf(mathService.multiply(a, b)));
         return response;
+    }
+
+    @PostMapping("/api/login/")
+    public void registerUser(@RequestBody Map<String, String> request) {
+        String username = request.get("username");
+        String password = request.get("password");
+
+        try(FileWriter dane = new FileWriter("users.txt", true)) {
+            dane.append("Username: " + username + ", Password: " + password + "\n");
+            dane.flush();
+        }
+        catch (IOException e) {
+            System.out.println("Nie dziala");
+            e.printStackTrace();
+        }
     }
 
 }
