fcb52d8eba92aba55201c34c2d619f7c960dde53
fkycia222 <fkycia@gmail.com>
2026-04-27 08:23:57 +0200

    Ubuntu dependiencies fix

--- frontend/electron/main.ts
+++ frontend/electron/main.ts
@@ -1,9 +1,16 @@
 import { app, BrowserWindow } from "electron";
 
+// app.disableHardwareAcceleration();
+
+// app.commandLine.appendSwitch("disable-gpu");
+// app.commandLine.appendSwitch("disable-software-rasterizer");
+// app.commandLine.appendSwitch("disable-accelerated-video-decode");
+// app.commandLine.appendSwitch("disable-features", "VaapiVideoDecoder,VaapiVideoDecodeLinuxGL");
+
 function createWindow() {
   const win = new BrowserWindow({
-    width: 600,
-    height: 400,
+    width: 1800,
+    height: 1000,
   });
 
   // Load Vite dev server
